df0f51dc1846556a5d4c5140be09512e93768474
mheydt <mike@heydt.org>
2018-02-27 23:58:36 -0600

    web tweaks

--- Deployment-Scripts/CSE/manual-build-web-image.ps1
+++ Deployment-Scripts/CSE/manual-build-web-image.ps1
@@ -4,5 +4,21 @@ wget https://raw.githubusercontent.com/MHWorkspaceCC/Workspace/master/Deployment
 wget https://raw.githubusercontent.com/MHWorkspaceCC/Workspace/master/Deployment-Scripts/CSE/install-octopusdsc.ps1 -OutFile install-octopusdsc.ps1
 wget https://raw.githubusercontent.com/MHWorkspaceCC/Workspace/master/Deployment-Scripts/CSE/configure-web-server-image.ps1 -OutFile configure-web-server-image.ps1
 
+$dbBackupsStorageAccountName = "stgdbbackupsss0p"
+$dbBackupsStorageAccountKey = "dMFiKWGj8AtVR1Tf4xTgWEEqdUS0wIX/iJU/VAGrDCX/G8YfkH1mZeQUDI6h0xKQWvlVwH16nDGmzNneiMP11w=="
+$webArchiveBlobName = "ws-client-web-6.0.0.502.zip"
+
+$backupStorageContext = New-AzureStorageContext -StorageAccountName $dbBackupsStorageAccountName -StorageAccountKey $dbBackupsStorageAccountKey
+Get-AzureStorageBlobContent -Blob $webArchiveBlobName -Container "current" -Destination "c:\web.zip" -Context $backupStorageContext
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip "c:\web.zip" "c:\"
 . .\configure-web-server-image.ps1
 Remove-Item -Path c:\config.log
